5091b73eacbfee52da91d292281bbacf83142be0
Siddharth2110 <45649060+Siddharth2110@users.noreply.github.com>
2020-06-13 15:09:51 +0530

    2 files- (a)main file to classify model
    export FLASK_APP=start.py
    export FLASK_ENV=development
    flask run

---
+++ FLASK_connect/funct.py
@@ -0,0 +1,25 @@
+from ReadClassify import Preprocess
+import pickle
+import numpy as np
+
+def temp(inputvalues):
+    m = Preprocess(inputvalues['To'], inputvalues['From'], inputvalues['Subject'], inputvalues['msgBody'])
+    return m
+#print(m)
+
+def add(n,m):
+    s=n+m
+    return s
+
+model=pickle.load(open('test_model.pkl','rb'))
+
+def pred(a,b,c):
+    a = np.asarray(a, dtype='float64')
+    b = np.asarray(b, dtype='float64')
+    c = np.asarray(c, dtype='float64')
+    p=model.predict([[a,b,c]])
+    res=int(p)
+    return res
+
+k=pred('3','7','9')
+print(k)
---
+++ FLASK_connect/start.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, jsonify, abort, request, redirect, url_for
+from ReadClassify import Preprocess
+from funct import add, temp, pred
+from testing import regressor
+from ProcessEmail import ProcessNewEmail
+
+app = Flask(__name__)
+
+counter = 0
+
+# def card_view(index):
+#   cards = db[index]
+#   return render_template("cards.html",
+#                          cards=cards,
+#                          index=index,
+#                          max_index=len(db) - 1)
+
+
+@app.route("/classified_class/<string:mesg>")
+def cls(mesg):
+    return render_template('outputclass.html', m=mesg)
+
+
+@app.route("/", methods=["GET", "POST"])
+def welcome():
+    if request.method == "POST":
+        inputvalues = {
+            "To": request.form['To'],
+            "From": request.form['From'],
+            "Subject": request.form['Subject'],
+            "Message": request.form['Message']
+        }
+        m = ProcessNewEmail(inputvalues['To'], inputvalues['From'], inputvalues['Subject'], inputvalues['Message'])
+        return redirect(url_for('cls', mesg=m))
+    else:
+        return render_template("home.html")
+
+
+@app.route("/test", methods=["GET", "POST"])
+def test():
+    if request.method == "POST":
+        m = pred(request.form['Experience'], request.form['test_score'], request.form['interview_score'])
+        return redirect(url_for('test_ans', ans=m))
+    else:
+        return render_template("test.html")
+
+
+@app.route("/test_ans/<int:ans>")
+def test_ans(ans):
+    return render_template('test_ans.html', ans=ans)
