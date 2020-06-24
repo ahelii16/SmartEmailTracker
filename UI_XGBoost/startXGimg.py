a345972906afcabc2df5adb116ac56c6f667abf0
Divya Agarwal <56511764+divya116@users.noreply.github.com>
2020-06-24 16:38:13 +0530

    dynamic XG boost UI

---
+++ UI_XGBoost/startXGimg.py
@@ -0,0 +1,247 @@
+# Wednesday 24th JUNE 2020 11 AM
+# load XG boost model
+# allows email to be uploaded as a file
+# allows one file attachment in email
+
+
+from flask import Flask, render_template, jsonify, abort, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+#from junecheckone import inputfunc
+from word2vec_xgb import inp
+from Train_Word2vec_XGBoost1 import train
+from PDFMinerParser import parsePDF, ValidExt
+from listenerpdfXG import HandleNewEmail, on_created
+
+
+import time
+import os
+import csv
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mails.sqlite3'
+app.config['SECRET_KEY'] = "random string"
+app.config['UPLOAD_FOLDER'] = "uploads"
+
+global myDict, mclass, tid
+
+db = SQLAlchemy(app)
+
+
+class mails(db.Model):
+    id = db.Column('mail_id', db.Integer, primary_key=True)
+    mfrom = db.Column(db.String(50))
+    mto = db.Column(db.String(50))
+    mdate = db.Column(db.String(20))
+    msubject = db.Column(db.String(200))
+    tid=db.Column(db.String(10))
+    mbody = db.Column(db.String(500))
+    mclass = db.Column(db.String(50))
+
+
+def __init__(self, mfrom, mto, mdate, msubject, tid, mbody, mclass):
+    self.mfrom = mfrom
+    self.mto = mto
+    self.mdate = mdate
+    self.msubject = msubject
+    self.tid=tid
+    self.mbody = mbody
+    self.mclass = mclass
+
+
+#not being used
+# write form input to text file so that listener can detect, classify, move to output class directory, write to DB
+def write_mail(mail):
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    f = open("./inputEmails/email_" + str(timestr) + '.txt', "w+")
+    f.write("To: %s \n" % mail.mto)
+    f.write("From: %s \n" % mail.mfrom)
+    #f.write("ID %s\n" % mail.mdate)
+    f.write("Subject: %s \n\n" % mail.msubject)
+    f.write("%s \n" % mail.mbody)
+    #f.write("Email_Body: %s \n" % mail.mbody)
+    #f.write("class: %s \n" % mail.m_class)
+    f.close()
+
+
+#To upload entrire email as a file
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file_1():
+    global myDict, mclass, tid
+    if request.method == 'POST':
+
+      if not request.files.get('file', None):
+        msg = 'Empty email or invalid attachment - no prediction!'
+        return render_template('index2.html',message=msg)
+
+      f = request.files['file']
+      print('File Uploaded=====')
+      f = request.files['file']
+      sfname = (secure_filename(f.filename))
+      timestr = time.strftime("%Y%m%d-%H%M%S")
+      fullname = str(timestr) + "_" +  sfname
+      f.save("./inputEmails/" + fullname)
+      '''
+      (secure_filename(f.filename))
+      timestr = time.strftime("%Y%m%d-%H%M%S")
+      fullname = str(timestr) + "_" + secure_filename(f.filename)
+      #f.save(os.path.join(app.config['UPLOAD_FOLDER'] fullname))
+      '''
+
+      to_add, from_add, receivedDate, sub, id, body, m_class = HandleNewEmail('inputEmails/' + fullname)
+      myDict = {
+          "From": from_add,
+          "To": to_add,
+          "Subject": sub,
+          "Message": body,
+          "Date": receivedDate
+      }
+      mclass=m_class
+      tid=id
+      print( myDict['From'], myDict['To'], myDict['Date'], myDict['Subject'], tid, myDict['Message'], mclass)
+      return render_template("index3.html", m=m_class)
+
+    else:
+      return render_template("index3.html")
+
+
+
+#Append emails from DB to training dataset
+@app.route('/retrain')
+def retrain():
+    with open('emaildataset.csv', 'a+') as f:
+        out = csv.writer(f)
+        ct=0
+        for mail in mails.query.all():
+            out.writerow([mail.mfrom, mail.mto, mail.msubject, mail.mbody, mail.tid, mail.mdate, mail.mclass])
+            ct = ct +1
+        db.session.query(mails).delete()
+        db.session.commit()
+        f.close()
+        msg = '' + str(ct) + ' mail(s) sent for retraining successfully!'
+        # run model again
+        train()
+    return render_template('index2.html',message=msg)
+
+
+#View all emails in DB
+@app.route('/show')
+def show_all():
+    return render_template('show_all.html', mails=mails.query.order_by(mails.id.desc()).all())
+
+
+#home page
+@app.route("/", methods=["GET", "POST"])
+def welcome():
+    global myDict, mclass, tid
+    if request.method == "POST":
+        myDict=inputvalues = {
+            "From": request.form['From'],
+            "To": request.form['To'],
+            "Subject": request.form['Subject'],
+            "Message": request.form['Message'],
+            "Date": request.form['date']
+        }
+
+        #handle file attachment in email from form
+        body1=""
+        if not request.files.get('file', None):
+            print('No file uploaded')
+        else:
+            f = request.files['file']
+            print('File Uploaded')
+
+            sfname = (secure_filename(f.filename))
+            timestr = time.strftime("%Y%m%d-%H%M%S")
+            fullname = str(timestr) + sfname
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], fullname))
+
+            #extract text from txt or pdf AND IMAGE, else "" returned
+            body1 = ValidExt(app.config['UPLOAD_FOLDER'] + '/' + fullname)
+
+            if (body1!=""):
+                body1 = '\n-----------Extracted from Attachment-----------\n' + body1
+
+        #append attchment txt to message body for prediction
+        inputvalues['Message'] = inputvalues['Message'] + str(body1)
+        print(inputvalues['Message'])
+
+
+        if (inputvalues['Subject']=="" and inputvalues['Message']==""):
+            msg = 'Empty email or invalid attachment - no prediction!'
+            return render_template('index2.html',message=msg)
+
+        #m_class, ID = inputfunc(inputvalues['To'], inputvalues['From'], inputvalues['Subject'], inputvalues['Message'])
+        m_class, ID = inp(inputvalues['To'], inputvalues['From'], inputvalues['Subject'], inputvalues['Message'])
+        mclass = m_class
+        tid = ID
+        return render_template("index3.html", m=m_class)
+
+    else:
+        return render_template("index3.html")
+
+
+@app.route('/wrong', methods=['GET', 'POST'])
+def wrong():
+    if request.method == "POST":
+        mclass = str(request.form.get("class", None))
+        if mclass == 'newclass':
+            return redirect(url_for('new_class'))
+        mail = mails()
+        print( "222222",myDict['From'], myDict['To'], myDict['Date'], myDict['Subject'], tid, myDict['Message'], mclass)
+        __init__(mail, myDict['From'], myDict['To'], myDict['Date'], myDict['Subject'], tid, myDict['Message'], mclass)
+        #write_mail(mail)
+        db.session.add(mail)
+        db.session.commit()
+        #time.sleep(4)
+        flash('Record was successfully added')
+        return redirect(url_for('show_all'))
+    else:
+        return render_template("dropdown.html")
+
+
+@app.route('/newclass', methods=['GET', 'POST'])
+def new_class():
+    if request.method == "POST":
+        mclass = str(request.form['NewClass'])
+        mail = mails()
+        __init__(mail, myDict['From'], myDict['To'], myDict['Date'], myDict['Subject'], tid, myDict['Message'], mclass)
+        #write_mail(mail)
+        db.session.add(mail)
+        db.session.commit()
+        # time.sleep(4)
+        flash('Record was successfully added')
+        return render_template("index3.html")
+    else:
+        return render_template("new_class.html")
+
+
+@app.route('/right')
+def right():
+    mail = mails()
+    __init__(mail, myDict['From'], myDict['To'], myDict['Date'], myDict['Subject'], tid, myDict['Message'], mclass)
+    #write_mail(mail)
+    db.session.add(mail)
+    db.session.commit()
+    return redirect(url_for('welcome'))
+
+
+@app.route('/thread', methods = ['GET','POST'])
+def findthread():
+    if request.method == "POST":
+        id = str(request.form['transacid'])
+        return render_template('show_all.html', mails=mails.query.filter(mails.tid == id).all())
+    else :
+        return render_template('findthread.html')
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
---
+++ UI_XGBoost/word2vec_xgb.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import numpy as np
+import joblib
+from wordfile import func
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import re
+import gensim.downloader as api
+from gensim.models.keyedvectors import Word2VecKeyedVectors
+import joblib
+from sklearn.decomposition import PCA
+from sklearn.metrics import accuracy_score
+from scipy import spatial
+import spacy
+import xgboost
+
+model = api.load('word2vec-google-news-300')
+nlp = spacy.load('en')
+
+my_stop = ["'d", "'ll", "'m", "'re", "'s", "'ve",'a','cc','subject','http', 'gbp', 'usd', 'eur', 'inr', 'cad', 'thanks', "acc", "id", 'account', 'regards', 'hi', 'hello', 'thank you', 'greetings', 'about','above', 'across','after','afterwards','against','alone','along','already','also','although','am','among', 'amongst','amount','an','and','another','any','anyhow','anyone','anything','anyway','anywhere','are','around','as', 'at','be','became','because','become','becomes','becoming','been','before','beforehand','behind','being','below', 'beside','besides','between','both','bottom','but','by','ca','call','can','could','did', 'do', 'does', 'doing', 'down', 'due', 'during', 'each', 'eight', 'either', 'eleven', 'else', 'elsewhere', 'every', 'everyone', 'everything', 'everywhere', 'fifteen', 'fifty', 'first', 'five', 'for', 'former', 'formerly', 'forty', 'four', 'from', 'front', 'further', 'get', 'give', 'go', 'had', 'has', 'have', 'he', 'hence', 'her', 'here', 'hereafter', 'hereby', 'herein', 'hereupon', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'however', 'hundred', 'i', 'if', 'in', 'indeed', 'into', 'is', 'it', 'its', 'itself', 'just', 'keep', 'last', 'latter', 'latterly', 'least', 'less', 'made', 'make', 'many', 'may', 'me', 'meanwhile', 'might', 'mine', 'more', 'moreover', 'mostly', 'move', 'much', 'must', 'my', 'myself', 'name', 'namely', 'neither', 'nevertheless', 'next', 'nine', 'no', 'nobody', 'now', 'nowhere', 'of', 'off', 'often', 'on', 'once', 'one', 'only', 'onto', 'or', 'other', 'others', 'otherwise', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 'part', 'per', 'perhaps', 'please', 'put', 'quite', 'rather', 're', 'really', 'regarding', 'same', 'say', 'see', 'seem', 'seemed', 'seeming', 'seems', 'serious', 'several', 'she', 'should', 'show', 'side', 'since', 'six', 'sixty', 'so', 'some', 'somehow', 'someone', 'something', 'sometime', 'sometimes', 'somewhere', 'still', 'such', 'take', 'ten', 'than', 'that', 'the', 'their', 'them', 'themselves', 'then', 'thence', 'there', 'thereafter', 'thereby', 'therefore', 'therein', 'thereupon', 'these', 'they', 'third', 'this', 'those', 'though', 'three', 'through', 'throughout', 'thru', 'thus', 'to', 'together', 'too', 'top', 'toward', 'towards', 'twelve', 'twenty', 'two', 'under', 'unless', 'until', 'up', 'upon', 'us', 'used', 'using', 'various', 'very', 'via', 'was', 'we', 'well', 'were', 'whatever', 'whence', 'whenever', 'whereafter', 'whereas', 'whereby', 'wherein', 'whereupon', 'wherever', 'whether', 'which', 'while', 'whither', 'whoever', 'whole', 'whom', 'whose', 'will', 'with', 'within', 'would', 'yet', 'you', 'your', 'yours', 'yourself', 'yourselves', '‘d', '‘ll', '‘m', '‘re', '‘s', '‘ve', '’d', '’ll', '’m', '’re', '’s', '’ve']
+
+def get_only_chars(text):
+    text = text.replace("-", " ")
+    text = text.replace("\t", " ")
+    text = text.replace("\n", " ")
+
+    text=text.rstrip()
+    text = re.sub(r'[^a-zA-Z]', ' ', text)
+    t = ""
+
+    for i in text.lower().split():
+        if func(i) is not None:
+            t += func(i) + " "
+        else :
+            t += i + " "
+
+    t = t.rstrip()
+
+    text = " ".join([i for i in t.lower().split()])
+    text = " ".join(token for token in text.split() if token not in my_stop)
+
+    doc = nlp(text)
+    normalized = " ".join(token.lemma_ for token in doc)
+    doc = " ".join(token.orth_ for token in nlp(normalized) if not token.is_punct | token.is_space)
+    return doc
+
+
+def transform_sentence(text, model):
+
+    def preprocess_text(raw_text, model=model):
+        raw_text = raw_text.split()
+
+        return list(filter(lambda x: x in model.vocab, raw_text))
+
+    tokens = preprocess_text(text)
+
+    if not tokens:
+        return np.zeros(model.vector_size)
+
+    text_vector = np.mean(model[tokens], axis=0)
+
+    return np.array(text_vector)
+
+#searches for transaction id in subject, returns '' if not found
+def findNum(st):
+    a = []
+    for word in st.split():
+        try:
+            a.append(int(word))
+        except ValueError:
+            pass
+
+    if (len(a)>0):
+        return str(a[0])
+    return ''
+
+le = joblib.load('./pkl_objects/labelencoder.pkl')
+clf = joblib.load('./pkl_objects/clf.pkl')
+#print(list(le.classes_))
+
+def inp(emailto, emailfrom, subj, bod):
+    #print('XG loaded!!!')
+    ID = findNum(subj)
+    text = subj + " " + bod
+    text = get_only_chars(text)
+    #print(text)
+    X_test_mean = np.array([transform_sentence(text, model)])
+    #print(X_test_mean)
+    y_pred = clf.predict(X_test_mean)
+    #print(y_pred)
+    return le.inverse_transform(y_pred - 1)[0] , ID
+
+#print(inp("fvf", "defrfg", "payment processed", "hi, the payment for acc 1234 for usd 3456 was paid successfully."))
+#print(inp("abc@abc.com", "def@def.com", "payment 1234 processed", "hi, the payment for acc 1234 for usd 3456 was paid successfully."))
